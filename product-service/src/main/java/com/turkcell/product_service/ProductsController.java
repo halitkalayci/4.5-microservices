eb04afac37785607df8d09aaab2cb2324883301b
halitklyc <halitenes.kalayci@gmail.com>
2025-10-05 11:40:17 +0300

    load balancing test

--- product-service/src/main/java/com/turkcell/product_service/ProductsController.java
+++ product-service/src/main/java/com/turkcell/product_service/ProductsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductsController {
     @GetMapping
     public String get() {
+        System.out.println("Bu konsoldaki product service çalıştı.");
         return "Merhaba, Product-Service";
     }
 }
